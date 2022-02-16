6617f86bed04d0e1540e2f151897374696577f90
ranja <seolin9340@gmail.com>
2022-02-16 14:31:35 +0900

    [Design]: Home 컴포넌트 구조 작성

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
 const Home = () => {
-  return <div style={{ border: '1px solid #ccc', height: '100%' }}>Home</div>;
+  const txt = 'WashingTon, DC';
+  return (
+    <div style={{ border: '1px solid #ccc', height: '100%', flex: '1' }}>
+      <div>
+        <h2>Search Results &apos;{txt}&apos;</h2>
+        <span>17 recent found</span>
+      </div>
+
+      <div>
+        <div style={{ display: 'flex' }}>
+          <img alt="" src="http://placehold.it/320x100" />
+          <div>
+            <p>10th aaaaa</p>
+            <h3>김창남</h3>
+            <ul>
+              <li>친구추가</li>
+              <li>채팅</li>
+              <li>친구네 집</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
