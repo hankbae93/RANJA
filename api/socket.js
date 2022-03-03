ecdfcda43184770acb9603dee8b2a8718e725a7f
ranja <seolin9340@gmail.com>
2022-03-03 11:13:39 +0900

    [Feat]: 실시간 채팅 기능 추가
    - api단 users 배열로 실시간 접속한 유저 구분 및 데이터 전송 로직  추가

--- api/socket.js
+++ api/socket.js
@@ -1,5 +1,33 @@
 const SocketIO = require("socket.io");
 
+const users = [];
+
+const addUser = ({ id, username, room }) => {
+	username = username.trim().toLowerCase();
+	room = room.trim().toLowerCase();
+
+	const existingUser = users.find(
+		(user) => user.room === room && user.username === username
+	);
+
+	if (!username || !room) return { error: "Username and room are required." };
+	if (existingUser) return { error: "Username is taken." };
+
+	const user = { id, username, room };
+
+	users.push(user);
+
+	return { user };
+};
+
+const removeUser = (id) => {
+	const index = users.findIndex((user) => user.id === id);
+
+	if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
 module.exports = (server, app) => {
 	const io = SocketIO(server, {
 		path: "/socket.io",
@@ -11,13 +39,36 @@ module.exports = (server, app) => {
 	const chat = io.of("/chat");
 
 	room.on("connection", (socket) => {
-		console.log("room 네임스페이스에 접속");
+		console.log(`${socket.id} "room 네임스페이스에 접속"`);
 		socket.on("disconnect", () => {
 			console.log("room 네임스페이스 접속 해제");
 		});
 	});
 
-	io.on("connect", (socket) => {
-		console.log("연결됏다");
+	chat.on("connection", (socket) => {
+		socket.on("join", (data) => {
+			const { username, room } = data;
+			const { error, user } = addUser({ id: socket.id, username, room });
+
+			if (error) {
+				return console.error(error);
+			}
+
+			socket.join(user.room);
+		});
+
+		socket.on("sendMessage", (message) => {
+			const user = getUser(socket.id);
+			console.log(message);
+			chat.to(user.room).emit("message", {
+				username: message.username,
+				chat: message.chat,
+			});
+			console.table(users);
+		});
+
+		socket.on("disconnect", () => {
+			removeUser(socket.id);
+		});
 	});
 };
--- client/src/components/chatting/Chat.tsx
+++ client/src/components/chatting/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import axios from '../../axios';
@@ -9,6 +9,7 @@ import Message from './Message';
 interface ChatMessageType {
   room?: any;
   user?: string;
+  username?: string;
   chat: string;
 }
 
@@ -21,15 +22,18 @@ const Chat = () => {
   const txtRef = useRef() as React.MutableRefObject<HTMLInputElement>;
 
   useEffect(() => {
-    socket.current = io('http://localhost:8000/chat', {
-      path: '/socket.io',
-      // transports: ['websocket'],
-    });
+    if (user && id) {
+      socket.current = io('http://localhost:8000/chat', {
+        path: '/socket.io',
+        // transports: ['websocket'],
+      });
 
-    socket.current.on('chat', (data: string) => {
-      console.log(data);
-    });
-  }, []);
+      socket.current.emit('join', { username: user?.username, room: id });
+      socket.current.on('message', (data: { chat: string; username: string }) => {
+        setMessages((prev) => prev.concat({ chat: data.chat, user: data.username }));
+      });
+    }
+  }, [user, id]);
 
   useEffect(() => {
     const getChatMessages = async () => {
@@ -40,23 +44,27 @@ const Chat = () => {
     getChatMessages();
   }, [id]);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
-    try {
-      await axios.post(`/chat/room/${id}/chat`, { room: id, chat: txtRef.current.value, user: user?.username });
-      setMessages((prev) => prev.concat({ room: null, chat: txtRef.current.value, user: user?.username }));
-    } catch (err) {
-      console.log(err);
-    }
-  };
+      try {
+        await axios.post(`/chat/room/${id}/chat`, { id, chat: txtRef.current.value, user: user?.username });
+        socket.current.emit('sendMessage', { chat: txtRef.current.value, user: user?.username });
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    [user],
+  );
 
   return (
     <ChatWrapper>
       <ChatLog>
-        {messages.map((message) => (
-          <Message txt={message.chat} isMe={false} />
-        ))}
+        {messages.map((data) => {
+          console.log(data);
+          return <Message txt={data.chat} isMe={user?.username === data.user} />;
+        })}
       </ChatLog>
 
       <ChatForm onSubmit={handleSubmit}>
