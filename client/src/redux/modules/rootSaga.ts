4876211cf5413314f45c466efbeb670c7d372a89
ranja <seolin9340@gmail.com>
2022-03-10 09:26:11 +0900

    [Chore]: 배포 및 개발모드 전환 세팅

--- client/src/redux/modules/rootSaga.ts
+++
@@ -1,7 +0,0 @@
-import { all } from 'redux-saga/effects';
-import { authSaga } from './auth';
-import { mapSaga } from './map';
-
-export default function* rootSaga() {
-  yield all([authSaga(), mapSaga()]);
-}
