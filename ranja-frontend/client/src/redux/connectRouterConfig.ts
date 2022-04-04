26123276ed23d3958f450c374e4a1df6a76dbdf3
ranja <seolin9340@gmail.com>
2022-04-04 10:25:39 +0900

    [Init] RANJA v2

--- ranja-frontend/client/src/redux/connectRouterConfig.ts
+++
@@ -1,7 +0,0 @@
-import { createReduxHistoryContext, reachify } from 'redux-first-history';
-// import { createWouterHook } from "redux-first-history/wouter";
-import { createBrowserHistory } from 'history';
-
-export const { createReduxHistory, routerMiddleware, routerReducer } = createReduxHistoryContext({
-  history: createBrowserHistory(),
-});
